b45420e8bb788a0cdbf3bdd2b84d1f40f85e0018
Mateo Bravo <mateobravo6@gmail.com>
2023-09-27 12:55:55 -0500

    Apliacion Perimetro Circulo

--- app/src/main/java/com/example/laboratorio3/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio3/MainActivity.kt
@@ -4,12 +4,26 @@ App Perimetro del circuilo
 
 package com.example.laboratorio3
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.laboratorio3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.PerimeterCircleButton.setOnClickListener {
+            val intent = Intent(this,PerimeterCircleActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }

6f703bf262afbc495f733c75e300b9b45b02ca52
gilber <gilber>
2023-09-27 11:42:48 -0500

    finish square area

--- app/src/main/java/com/example/laboratorio3/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio3/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.laboratorio3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.laboratorio3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding=ActivityMainBinding.inflate(layoutInflater)
+        var view=mainBinding.root
+        setContentView(view)
+
+        mainBinding.buttonSquareArea.setOnClickListener {
+            val intent = Intent(this,SquareAreaActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
